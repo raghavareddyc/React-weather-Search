830fae9207630290dabd99944b8730c69b4636a6
raghavareddyc <crr933@gmail.com>
2018-10-02 16:14:20 -0500

    WeatherSearch

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY='9d506fef62dc7b2b8dd134d828644c67';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER='FETCH_WEATHER';//to keep our action type consistent b/w action creators and reducers
+
+export function fetchWeather(city){
+  const url=`${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  console.log(request) + 'invalid');
+
+  console.log('Request',request);
+
+  return {
+    type:FETCH_WEATHER,
+    payload:request
+  };
+}
---
+++ src/containers/weather_list.js
@@ -0,0 +1,52 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart.js';
+import GoogleMap from '../components/google_map.js';
+
+class WeatherList extends Component{
+renderWeather(cityData){
+
+const name =cityData.city.name;
+const temps=cityData.list.map(weather=>weather.main.temp);
+const pressures=cityData.list.map(weather=>weather.main.pressure);
+const humidities=cityData.list.map(weather=>weather.main.Humidity);
+// const lon = cityData.city.coord.lon;
+// const lat = cityData.city.coord.lat;
+const {lon, lat}= cityData.city.coord;
+
+  return(
+    <tr key={name}>
+      <td><GoogleMap lon={lon} lat={lat} /></td>
+      <td><Chart data={temps} color="orange" units="k" /></td>
+      <td><Chart data={pressures} color="green" units="hPa" /></td>
+      <td><Chart data={humidities} color="black" units="%"/></td>
+    </tr>
+  );
+
+}
+
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+          <th>City</th>
+          <th>Temperature (k)</th>
+          <th>Pressure (hPa)</th>
+          <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+
+function mapStateToProps({weather}){
+  return {weather}; // {weather} ==={weather:weather}
+}
+
+export default connect(mapStateToProps)(WeatherList);
